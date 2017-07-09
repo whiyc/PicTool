2ffef8ec7eed7de87a39eeee409bdbf5f601c3ec
whiyc <whiycly@163.com>
2017-07-10 02:20:46 +0800

    first commit

---
+++ 图片处理工具1.0/图片处理工具1.0/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace 图片处理工具1._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.开始 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(371, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(448, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "初始化";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(33, 311);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(721, 268);
+            this.listBox1.TabIndex = 2;
+            // 
+            // 开始
+            // 
+            this.开始.Location = new System.Drawing.Point(567, 61);
+            this.开始.Name = "开始";
+            this.开始.Size = new System.Drawing.Size(75, 23);
+            this.开始.TabIndex = 3;
+            this.开始.Text = "开始";
+            this.开始.UseVisualStyleBackColor = true;
+            this.开始.Click += new System.EventHandler(this.开始_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "宽";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 208);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "尺寸";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "高";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(72, 104);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(179, 21);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "1100,420,750";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(72, 155);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(179, 21);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "1390,531,0";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(72, 205);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(179, 21);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.Text = "300,80,1000";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(331, 158);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(213, 21);
+            this.textBox5.TabIndex = 11;
+            this.textBox5.Text = "1600";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(272, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "限制高度";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(793, 608);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.开始);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button 开始;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ 图片处理工具1.0/图片处理工具1.0/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 图片处理工具1._0
+{
+    public partial class Form1 : Form
+    {
+        
+        //图片目录总集
+        private List<String> picLists = new List<String>() ;
+        //图片宽,高,大小
+        private Dictionary<int, List<int>> param = new Dictionary<int, List<int>>();
+        //图片,高,大小
+        //private List<int> heightAndSize = new List<int>();
+        //图片后缀名
+        private List<String> picExten = new List<string>();
+        //图片固定集合
+        private List<String> picNames = new List<string>();
+        //检查图片是否缺图
+        private List<String> regularPicNames = new List<string>();
+
+        //限制高度
+        private String picLimteHeight;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            
+            InitInfo();
+            String dir = textBox1.Text;
+            DirectoryInfo folders = new DirectoryInfo(dir);
+            //遍历文件夹
+            ListFiles(folders);
+
+        }
+
+        private void InitInfo()
+        {
+
+            InitPicName();
+
+            InitPicInfo();
+
+            InitPicExten();
+
+        }
+
+        private void InitPicExten()
+        {
+            picExten.Add(".png");
+            picExten.Add(".jpg");
+            picExten.Add(".jpeg");
+            picExten.Add(".bmp");
+            picExten.Add(".gif");
+            picExten.Add(".JPG");
+            picExten.Add(".PNG");
+            picExten.Add(".JPEG");
+            picExten.Add(".BMP");
+            picExten.Add(".GIF");
+            
+
+        }
+
+        private void InitPicName()
+        {
+
+            regularPicNames.Add("1");
+            regularPicNames.Add("2");
+            regularPicNames.Add("3");
+            regularPicNames.Add("4");
+            regularPicNames.Add("5");
+            regularPicNames.Add("7");
+            regularPicNames.Add("15");
+            regularPicNames.Add("16");
+            regularPicNames.Add("601");
+
+            picNames.Add("1");
+            picNames.Add("2");
+            picNames.Add("3");
+            picNames.Add("4");
+            picNames.Add("5");
+            picNames.Add("7");
+            picNames.Add("15");
+            picNames.Add("16");
+            
+
+            for (int y = 601; y <= 650; y++)
+            {
+                picNames.Add(y.ToString());
+            }
+        }
+
+        private void InitPicInfo()
+        {
+
+            String picWidth = textBox2.Text;
+            String picHeight = textBox3.Text;
+            String picSize = textBox4.Text;
+            picLimteHeight = textBox5.Text;
+
+            String[] widthStr = picWidth.Split(',');
+            String[] heightStr = picWidth.Split(',');
+            String[] sizeStr = picWidth.Split(',');
+
+            for (int x = 0; x < heightStr.Length; x++) {
+                List<int> heightAndSize = new List<int>();
+                heightAndSize.Add(int.Parse(heightStr[x]));
+                heightAndSize.Add(int.Parse(sizeStr[x]));
+                param.Add(int.Parse(widthStr[x]),heightAndSize);
+            }
+            
+
+            //Console.WriteLine(widthStr[0]+ widthStr[1]+ widthStr[2]);
+
+        }
+
+        private void ListFiles(FileSystemInfo info)
+        {
+
+            if (!info.Exists) return;
+
+            DirectoryInfo dir = info as DirectoryInfo;
+
+            if (dir == null) return;
+
+            FileSystemInfo[] files = dir.GetFileSystemInfos();
+
+            List<String> regularPicName = new List<string>();
+            for (int z = 0;z<regularPicNames.Count;z++) {
+
+                regularPicName.Add(Path.GetDirectoryName(files.ElementAt(0).FullName)+"\\"+regularPicNames.ElementAt(z));
+            }
+           
+            for (int i = 0; i < files.Length; i++)
+            {
+                FileInfo file = files[i] as FileInfo;
+                
+                if (file != null) {
+         
+                    if (picExten.Contains(file.Extension))
+                    {
+                        picLists.Add(file.FullName);
+                    }
+                    else {
+                        listBox1.Items.Add(file.FullName);
+                    }
+                    
+                    
+                }
+                else ListFiles(files[i]);
+            }
+
+            if (regularPicNames.Count > 0) {
+
+                foreach (String picMame in regularPicNames) {
+
+                    listBox1.Items.Add(files.ElementAt(0).FullName);
+                }
+            }
+
+        }
+
+        private void 开始_Click(object sender, EventArgs e)
+        {
+            checkPic();
+        }
+
+        private void checkPic()
+        {
+            
+        }
+    }
+}
